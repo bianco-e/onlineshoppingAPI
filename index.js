b333cfd21f0ef57fede0e5b42e6d504e5fd7e46b
Bianco <bianco.emiliozzi@gmail.com>
2020-09-11 18:35:44 -0300

    refactored code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const Product = require("./models/productModel");
 const Categories = require("./models/categoriesModel");
 const Message = require("./models/messageModel");
 const Config = require("./models/configModel");
-const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/onlineshopping", {
   useNewUrlParser: true,
@@ -23,78 +23,110 @@ db.once("open", () => {
 });
 
 const collsData = {
-  categories: { model: Categories },
-  config: { model: Config },
-  messages: { model: Message, addEndpoint: "/addMessage" },
-  products: { model: Product, addEndpoint: "/addProduct" },
+  categories: { Model: Categories },
+  config: { Model: Config },
+  messages: { Model: Message },
+  products: { Model: Product },
 };
 
-const getDocsFromCollection = (collection, callback) => {
-  app.get(`/${collection}`, function (req, res) {
-    collsData[collection].model.find({}, function (err, docs) {
-      callback ? res.send(callback(docs)) : res.send(docs);
-    });
-  });
+const getDocById = (collection) => {
+  app.get(`/${collection}/id/:id`, (req, res) =>
+    collsData[collection].Model.findById(req.params.id, (err, doc) =>
+      res.send(doc)
+    )
+  );
 };
 
-const getPromProducts = () => {
-  app.get("/promProducts", function (req, res) {
-    Product.find({ prom: true }, function (err, docs) {
-      res.send(docs);
-    });
-  });
+const getDocsFromCollection = (collection) => {
+  app.get(`/${collection}`, (req, res) =>
+    collsData[collection].Model.find({}, (err, docs) => res.send(docs))
+  );
 };
 
 const addNewDoc = (collection) => {
-  app.post(collection.addEndpoint, jsonParser, function (req, res) {
-    const newDoc = new collection.model(req.body);
+  const { Model } = collsData[collection];
+  app.post(`/${collection}/add`, (req, res) => {
+    const newDoc = new Model(req.body);
     newDoc.save((err, addedDoc) => {
       if (err) return console.log(err);
-      console.log("Added", addedDoc);
       res.statusCode = 200;
       res.end();
     });
   });
 };
 
-app.get("/", function (req, res) {
-  res.send("Hi!");
-});
+const deleteDoc = (collection) => {
+  const { Model } = collsData[collection];
+  app.delete(`/${collection}/delete`, (req, res) => {
+    Model.deleteOne({ _id: req.body.id }, (err) => {
+      if (err) return console.log(err);
+      res.statusCode = 200;
+      res.end();
+    });
+  });
+};
 
+const editDoc = (collection) => {
+  const { Model } = collsData[collection];
+  app.put(`/${collection}/edit`, (req, res) => {
+    Model.replaceOne({ _id: req.body.id }, req.body.newDoc, {}, (err) => {
+      if (err) return console.log(err);
+      res.statusCode = 200;
+      res.end();
+    });
+  });
+};
+
+const getByNameContains = (collection) => {
+  const { Model } = collsData[collection];
+  app.get(`/${collection}/search/:keyword`, (req, res) => {
+    Model.find(
+      { name: { $regex: req.params.keyword, $options: "i" } },
+      (err, docs) => {
+        if (err) return console.log(err);
+        res.send(docs);
+      }
+    );
+  });
+};
+
+const getProductsWhere = () => {
+  app.get(`/products/where/:property/:value`, (req, res) =>
+    Product.find(
+      { [req.params.property]: { $eq: req.params.value } },
+      (err, docs) => res.send(docs)
+    )
+  );
+};
+
+const getCategoriesNames = () => {
+  app.get(`/categories/names`, (req, res) =>
+    Categories.find({}, (err, docs) =>
+      res.send(docs[0].categories.map((cat) => cat.name))
+    )
+  );
+};
+
+app.get("/", (req, res) => res.send("Hi!"));
+
+// All collections ----
+Object.keys(collsData).forEach((collection) => {
+  getDocsFromCollection(collection);
+  editDoc(collection);
+});
 // Products ----
-
-getDocsFromCollection("products");
-
-app.get("/products/:id", function (req, res) {
-  Product.findById(req.params.id, (err, doc) => {
-    res.send(doc);
-  });
-});
-
-getPromProducts();
-addNewDoc(collsData.products);
-
+getDocById("products");
+addNewDoc("products");
+deleteDoc("products");
+getByNameContains("products");
+getProductsWhere();
 // Categories ----
-
-getDocsFromCollection("categories", (docs) => docs[0].categories);
-
-app.get("/categories/names", function (req, res) {
-  Categories.find({}, function (err, docs) {
-    res.send(docs[0].categories.map((doc) => doc.name));
-  });
-});
-
+getDocById("categories");
+getCategoriesNames();
 // Messages ----
-
-getDocsFromCollection("messages");
-addNewDoc(collsData.messages);
-
+addNewDoc("messages");
+deleteDoc("messages");
 // Config ----
-
-getDocsFromCollection("config", (docs) => docs[0]);
-
+getDocById("config");
 // -----
-
-app.listen(5000, () => {
-  console.log("On port 5000");
-});
+app.listen(5000, () => console.log("On port 5000"));
